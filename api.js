fd3fc664de94d6fe9dd430ce1fa8eaef9587fe18
Kaki LAI <kakikakikakiuk@gmail.com>
2025-05-22 15:04:33 +0100

    View an individual article

--- api.js
+++ api.js
@@ -1,10 +1,18 @@
-import axios from "axios"
+import axios from "axios";
 
 const ncNewsApi = axios.create({
-    baseURL:"https://nc-news-api-szrs.onrender.com/api"
-})
+  baseURL: "https://nc-news-api-szrs.onrender.com/api",
+});
 export const getArticles = () => {
-    return ncNewsApi.get("/articles").then((res) =>{
-        return res.data.articles
-    })
-}
+  return ncNewsApi.get("/articles").then((res) => {
+    return res.data.articles;
+  });
+};
+
+export const getArticleById = (article_id) => {
+  return ncNewsApi.get(`/articles/${article_id}`).then((res) => {
+    
+
+    return res.data.article;
+  });
+};
--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,7 +1,45 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getArticleById } from "../../api";
+import dayjs from "dayjs";
 
-export default function ArticlePage(){
-    const {article_id}=useParams()
-    return <h2>You are viewing {article.id}</h2>
+export default function ArticlePage() {
+  const { article_id } = useParams();
+  const [article, setArticle] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getArticleById(article_id)
+      .then((data) => {
+        setArticle(data);
+      })
+      .catch((err) => {
+        setIsError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [article_id]);
+  
+  if (isLoading) {
+    return <p>⏳Article is Loading ... 🥱</p>;
+  }
+  if (isError) {
+    return <p>OMG 🤯 Something went wrong</p>;
+  }
+console.log(article);
+
+  return (
+    <section>
+      <h2>{article.title}</h2>
+      <p>{article.topic}</p>
+      <p>{article.body}</p>
+      <img src={article.article_img_url} alt="image of the article" width={30} height={30} />
+      <p>
+        | votes{ article.votes} | 🗓️ {dayjs(article.created_at).format(" DD MMM YYYY ddd")} | 🖋️By {article.author} 
+      </p>
+    </section>
+  );
 }
-   
